f545c847d39efaa23ecfb698291bcccf4c25310d
shlomyli <shlomyli@users.noreply.github.com>
2014-05-29 16:06:14 +0300

    Update ui.R

--- WDI/ui.R
+++ WDI/ui.R
@@ -1,1 +1,47 @@
+## Shiny example - WDI
+## Shlomi Lifshits (2014)
+## shlomi.lifshits@yahoo.com
 
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    titlePanel("Rate of change in number of internet users across the world"),
+    withMathJax(),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("country", label="Country:",choices=countries,selected="IL"),
+        helpText("Selected:"),
+        htmlOutput("country.img"),  
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        helpText("Created by Shlomi Lifshits (2014)"),
+        helpText(HTML("Powered by <a href = \"http://www.rstudio.com/shiny/\">shiny</a>.")),
+        img(src="logo.jpg",height=52,width=52) 
+      ),
+      mainPanel(        
+        fluidRow(
+          column(3,
+                 verbatimTextOutput("text")
+          ),
+          column(3,
+                 img(src=WDI.LOGO.URL,height=52, width=52),
+                 a("http://data.worldbank.org/indicator")
+          )
+        ),
+        htmlOutput("view"),
+        p("Furmula \\(\\frac{x}{y} \\cdot 100\\), where x is the total number of users and y is the total population size.")
+      )
+    )
+  )
+)
